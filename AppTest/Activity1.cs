f6108a29548944fa0768c87bf789d6ce7a3c091a
Nabil Elidrissi <brqoqa12>
2021-06-17 23:15:04 +0100

    app test

---
+++ AppTest/Activity1.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppTest
+{
+    [Activity(Label = "Home")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.HomeScreen);
+            // Create your application here
+            var butt1 = FindViewById<ImageButton>(Resource.Id.imageView1);
+            butt1.Click += (e, o) =>
+            {
+                Toast.MakeText(this, string.Format("image clicked"), ToastLength.Short).Show();
+            };
+            
+        }
+        
+       
+    }
+}
